f0607c73a764d14c5277ddc8dce37243ca4acab1
Mavrick Laakso <laakso.mavrick@gmail.com>
2024-03-04 16:04:41 -0500

    Initial Commit

---
+++ Hugelifts/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  Hugelifts
+//
+//  Created by Mavrick Laakso on 2024-03-04.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
